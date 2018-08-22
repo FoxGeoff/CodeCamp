36874f3b8020b8ad71a6d12394065a63aa3008b5
FoxGeoff <fox.geoffrey@gmail.com>
2018-08-22 12:06:01 -0700

    Change to Item-Mock Service

--- src/app/items-list/items-list.component.ts
+++ src/app/items-list/items-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { Item } from '../shared';
+import { Item, ItemsMockService } from '../shared';
 
 @Component({
   selector: 'app-items-list',
@@ -12,7 +12,7 @@ export class ItemsListComponent implements OnInit {
   @Output() selected = new EventEmitter();
   @Output() deleted = new EventEmitter();
 
-  constructor() {
+  constructor(private itemsService: ItemsMockService) {
   }
 
   ngOnInit() {
@@ -20,50 +20,8 @@ export class ItemsListComponent implements OnInit {
   }
 
   getItems(): Item[] {
-    return [
-      { "id": 0, "name": "Thing item", "price": 20.56, "description": "This is a thing" },
-      { 
-        "id": 1, 
-        "name": "Red Item", 
-        "price": 100, 
-        "description": "This is a red item" 
-      },
-      {
-        "id": 2,
-        "name": "Orange Item edited",
-        "price": 200,
-        "description": "This is an orange item"
-      },
-      {
-        "id": 3,
-        "name": "Yellow Item",
-        "price": 300,
-        "description": "This is a yellow item"
-      },
-      {
-        "id": 4,
-        "name": "Green Item",
-        "price": 400,
-        "description": "This is a green item"
-      },
-      {
-        "id": 5,
-        "price": 500,
-        "name": "Blue Item",
-        "description": "This is a blue item"
-      },
-      {
-        "id": 6,
-        "name": "Indigo Item",
-        "price": 600,
-        "description": "This is a indigo item"
-      },
-      {
-        "id": 7,
-        "name": "Violet Item",
-        "price": 700,
-        "description": "This is a violet item"
-      }
-    ];
+
+    return this.itemsService.all();
+ 
   }
 }
--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,4 +1,5 @@
-//export { ItemsService } from './items.service';
+export { ItemsService } from './items.service';
+export { ItemsMockService } from './items-mock.service';
 export { Item } from './item.model';
 //export { WidgetsService } from './widgets.service';
 export { Widget } from './widget.model';
